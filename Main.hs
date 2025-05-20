56cf904e98aeee7cb1babed2ebd1cd41de0fd688
IsacEkeroth <isac.ekeroth@icloud.com>
2025-05-20 17:52:45 +0200

    Refactor main function to build AA tree and print statistics

--- Main.hs
+++ Main.hs
@@ -1,5 +1,12 @@
-import AATree ()
+import AATree
 
+{-
+  to run:
+  $ ghc -e main Main.hs < swahili-small.txt
+
+  to compile and run:
+  $ ghc -O Main.hs && ./Main < swahili-small.txt
+-}
 --------------------------------------------------------------------------------
 
 main :: IO ()
@@ -8,10 +15,25 @@ main = do
 
   -- split the data into words and build an AA tree
   -- use foldl
-  undefined
+  let wordsList = words contents
+      aatree = foldl (\t x -> insert x t) emptyTree wordsList
 
   -- calculate and print statistics
   -- use fromIntegral/ceiling/logBase
-  undefined
+
+  -- Size: 12582
+  -- Height: 18
+  -- Optimal height:
+  -- Height / Optimal height: 1.3846153846153846
+  -- checkTree: True
+  -- First 20 words: A ABP AC AD ADLugha ADNa ADR ADSL ADWabantu ADwaarabu AIDS AL ...
+  putStrLn $ "Size: " ++ show (size aatree)
+  putStrLn $ "Height: " ++ show (height aatree)
+  let n = size aatree
+      optimalHeight = ceiling (logBase 2 (fromIntegral n + 1))
+  putStrLn $ "Optimal height: " ++ show optimalHeight
+  putStrLn $ "Height / Optimal height: " ++ show (fromIntegral (height aatree) / fromIntegral optimalHeight)
+  putStrLn $ "checkTree: " ++ show (checkTree aatree)
+  putStrLn $ "First 20 words: " ++ show (take 20 $ inorder aatree)
 
 --------------------------------------------------------------------------------
